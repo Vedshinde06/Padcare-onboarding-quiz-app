566f86f987bd6e24efc61cca426550903cc7f806
Vedshinde06 <vedantshinde501@gmail.com>
2026-02-27 12:38:12 +0530

    updates

--- PadcareGPT/padcare_hr_bot/app/ingestion/chunker.py
+++
@@ -1,9 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-def chunk_documents(documents):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=150
-    )
-
-    return splitter.split_documents(documents)
